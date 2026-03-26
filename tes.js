d33e3b4fd6573ceca43b16cda56f59a4b22d5676
putriques <kokushibosol5@gmail.com>
2026-03-26 03:11:58 -0300

    img

---
+++ tes.js
@@ -0,0 +1,15 @@
+// Verificação soma de numeros maior que
+
+let a = 5
+let b = 6
+let c = 25
+
+const veri = a+b
+
+if (veri > c) {
+    console.log(veri + " é maior que " + c);
+    
+} else {
+    console.log(veri + " é menor que " + c);
+    
+}
